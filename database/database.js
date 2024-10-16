afaa578dd068b2a9f33aa5b5272dad1e845da6b5
gayangg <gayang.fi@gmail.com>
2024-10-16 11:31:09 +0300

    DB issue try2

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
-//import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
-import { postgres } from "../deps.js";
+import {postgres} from "../deps.js";
 
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
   sql = postgres(Deno.env.get("DATABASE_URL"));
+
 } else {
   sql = postgres({});
 }
--- deps.js
+++ deps.js
@@ -1,4 +1,3 @@
-//import "https://deno.land/x/dotenv@v3.2.0/load.ts";
-
 import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
-export { postgres };
+
+export {postgres}
