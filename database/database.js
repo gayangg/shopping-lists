e2796c68bd194344a40298fdba549f1bc6cfa6d1
gayangg <gayang.fi@gmail.com>
2024-10-16 09:00:55 +0300

    DB issue sort

--- database/database.js
+++ database/database.js
@@ -1,17 +1,11 @@
-import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+//import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+import { postgres } from "../deps.js";
 
 let sql;
 if (Deno.env.get("DATABASE_URL")) {
   sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
   sql = postgres({});
-  // sql = postgres({
-  //   hostname: <dbUrl>,
-  //   database: <dbname>,
-  //   user: <user>,
-  //   password: <pass>,
-  //   port: <port>,
-  // });
 }
 
 export { sql };
--- deps.js
+++ deps.js
@@ -1,1 +1,4 @@
-import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+//import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
+export { postgres };
